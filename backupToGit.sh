7b0da45811895de90e421415a08a5eb6b12e8cdb
Михаил Буяло <mk@buialo.com>
2021-07-17 01:07:15 +0200

    2021-07-17 01:07:15

--- backupToGit.sh
+++ backupToGit.sh
@@ -5,19 +5,19 @@ message=$(date '+%Y-%m-%d %H:%M:%S')
 tar czf node-red.tar.gz node-red/
 tar czf deconz.tar.gz deconz/
 # Encrypt containers data
-gpg -r 89E7B23D -e node-red.tar.gz
-gpg -r 89E7B23D -e deconz.tar.gz
+gpg -r 89E7B23D -e node-red.tar.gz -y
+gpg -r 89E7B23D -e deconz.tar.gz -y
 
-# stage all changes
-# git add .
-# echo "Staged all files"
+stage all changes
+git add .
+echo "Staged all files"
 
-# # add commit
-# git commit -m "$message"
-# echo "Added the commit with message"
+# add commit
+git commit -m "$message"
+echo "Added the commit with message"
 
-# # get current branch and push
+# get current branch and push
 # current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-# git push origin backup
-# echo "Push changes to '$current_branch' branch"
+git push origin backup
+echo "Push changes to backupß branch"
 
