a5c567283cd60250a3064a20316c2e5ff03b273c
Михаил Буяло <mk@buialo.com>
2021-07-17 01:03:16 +0200

    added archiving,compression and encryption

--- backupToGit.sh
+++ backupToGit.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 message=$(date '+%Y-%m-%d %H:%M:%S')
+# Archive and compress containers data
+tar czf node-red.tar.gz node-red/
+tar czf deconz.tar.gz deconz/
+# Encrypt containers data
+gpg -r 89E7B23D -e node-red.tar.gz
+gpg -r 89E7B23D -e deconz.tar.gz
 
 # stage all changes
-git add .
-echo "Staged all files"
+# git add .
+# echo "Staged all files"
 
-# add commit
-git commit -m "$message"
-echo "Added the commit with message"
+# # add commit
+# git commit -m "$message"
+# echo "Added the commit with message"
 
-# get current branch and push
-current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-git push origin "$current_branch"
-echo "Push changes to '$current_branch' branch"
+# # get current branch and push
+# current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+# git push origin backup
+# echo "Push changes to '$current_branch' branch"
 
