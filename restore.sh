4b4b515ac74d122df62098b8ccda126b0be2f406
Михаил Буяло <mk@buialo.com>
2021-07-17 01:49:54 +0200

    added evrything to gitignore

--- restore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Decrypt files 
-gpg -d node-red.tar.gz.gpg >node-red.tar.gz
-gpg -d deconz.tar.gz.gpg >deconz.tar.gz
-# Unarchive and uncompresss files
-tar xvzf node-red.tar.gz
-tar xvzf deconz.tar.gz
